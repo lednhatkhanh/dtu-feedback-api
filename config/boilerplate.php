26aaaa1ea6713874df42b69a5cc7c6a9cd0d3257
lednhatkhanh <lednhatkhanh@gmail.com>
2017-03-13 20:51:33 +0700

    Password constraint

--- config/boilerplate.php
+++ config/boilerplate.php
@@ -7,7 +7,7 @@ return [
         'validation_rules' => [
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required',
+            'password' => 'required|min:6',
             'avatar' => 'sometimes|required|image'
         ]
     ],
