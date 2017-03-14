4f3c0da0d8e8a6d6abf0249b770ea415370ca98c
lednhatkhanh <lednhatkhanh@gmail.com>
2017-03-14 12:54:59 +0700

    Add role when logging

--- app/Api/V1/Controllers/SignUpController.php
+++ app/Api/V1/Controllers/SignUpController.php
@@ -32,7 +32,7 @@ class SignUpController extends Controller
             throw new HttpException(500);
         }
 
-        //$user->assignRole('admin');
+        $user->assignRole('user');
 
         if(!Config::get('boilerplate.sign_up.release_token')) {
             return response()->json([
