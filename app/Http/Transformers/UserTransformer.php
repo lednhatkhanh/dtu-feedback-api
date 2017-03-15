8c4d3e2d1d7fbec22baeef044b523f9aed9e4d82
lednhatkhanh <lednhatkhanh@gmail.com>
2017-03-15 19:21:47 +0700

    Rewrite user transformer

--- app/Http/Transformers/UserTransformer.php
+++ app/Http/Transformers/UserTransformer.php
@@ -10,11 +10,22 @@ namespace App\Http\Transformers;
 
 
 use App\User;
+use Carbon\Carbon;
 use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
     public function transform(User $user) {
-        return $user;
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'avatar' => $user->avatar,
+            'roles' => $user->roles->pluck('name'),
+            'created_at' => Carbon::createFromFormat('Y-m-d H:m:s', $user->created_at)
+                ->format('Y-m-d H:m'),
+            'updated_at' => Carbon::createFromFormat('Y-m-d H:m:s', $user->updated_at)
+                ->format('Y-m-d H:m'),
+        ];
     }
 }
